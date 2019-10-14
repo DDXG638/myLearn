086ca95e8bc23da8670ab8c8b6363de55ad50294
xiaodongrui <981926563@qq.com>
2019-10-14 12:39:05 +0800

    promise练习

--- mianshi/promise/index.js
+++ mianshi/promise/index.js
@@ -41,27 +41,29 @@ class DPromise {
         // 一开始我将resolve 和 reject 方法写成了DPromise的成员函数的位置，后来运行报错了，
         // this指向错误，因为这两个函数是以参数的形式提供给callback调用的，
         // 在外面调用这两个函数的时候this指向的不是DPromise，所以函数里面使用this的地方都会出错
-        let resolve = value => {
+        // 使用成员方法的话可以使用bind方法来指定this的指向，callback(this.resolve.bind(this), this.reject.bind(this))
+        // let resolve = value => {
+        //     console.log('resolve-this', this);
 
-            /**
-             * 这里必须要使用异步操作，我这里简单地使用setTimeout代替，
-             * 用户在new Promise的时候很可能写的是同步代码，如果这里没有处理的话，同步代码就会在then方法之前执行了
-             * 没有先执行then方法的话，同步代码执行的时候下面的resolveCbs数组就会是空的，就无法执行resolve回调函数，
-             * 这个promise写了跟没写一样
-             */
-            setTimeout(() => {
-                // 只有状态为PENDING才能转换为FULFILLED
-                if (this.state === PENDING) {
-                    this.state = FULFILLED
+        //     /**
+        //      * 这里必须要使用异步操作，我这里简单地使用setTimeout代替，
+        //      * 用户在new Promise的时候很可能写的是同步代码，如果这里没有处理的话，同步代码就会在then方法之前执行了
+        //      * 没有先执行then方法的话，同步代码执行的时候下面的resolveCbs数组就会是空的，就无法执行resolve回调函数，
+        //      * 这个promise写了跟没写一样
+        //      */
+        //     setTimeout(() => {
+        //         // 只有状态为PENDING才能转换为FULFILLED
+        //         if (this.state === PENDING) {
+        //             this.state = FULFILLED
 
-                    this.result = value
-                    // 调用resolve数组中的回调函数
-                    this.resolveCbs.forEach(item => {
-                        this.result = item(this.result)
-                    })
-                }
-            }, 0)
-        }
+        //             this.result = value
+        //             // 调用resolve数组中的回调函数
+        //             this.resolveCbs.forEach(item => {
+        //                 this.result = item(this.result)
+        //             })
+        //         }
+        //     }, 0)
+        // }
 
         let reject = error => {
             setTimeout(() => {
@@ -72,13 +74,20 @@ class DPromise {
 
                     // 调用resolve数组中的回调函数
                     this.rejectCbs.forEach(item => {
-                        this.reasen = item(this.reasen) || null
+                        if (typeof item === 'function') {
+                            this.reasen = item(this.reasen) || null
+                        }
                     })
                 }
             }, 0)
         }
 
-        callback(resolve, reject)
+        // 捕获到错误立即reject
+        try {
+            callback(this.resolve.bind(this), reject)
+        } catch(err) {
+            reject(err)
+        }
     }
 
     // Promise.all方法
@@ -107,6 +116,31 @@ class DPromise {
         })
     }
 
+    resolve(value) {
+        // console.log('resolve-this', this);
+
+        /**
+         * 这里必须要使用异步操作，我这里简单地使用setTimeout代替，
+         * 用户在new Promise的时候很可能写的是同步代码，如果这里没有处理的话，同步代码就会在then方法之前执行了
+         * 没有先执行then方法的话，同步代码执行的时候下面的resolveCbs数组就会是空的，就无法执行resolve回调函数，
+         * 这个promise写了跟没写一样
+         */
+        setTimeout(() => {
+            // 只有状态为PENDING才能转换为FULFILLED
+            if (this.state === PENDING) {
+                this.state = FULFILLED
+
+                this.result = value
+                // 调用resolve数组中的回调函数
+                this.resolveCbs.forEach(item => {
+                    if (typeof item === 'function') {                        
+                        this.result = item(this.result)
+                    }
+                })
+            }
+        }, 0)
+    }
+
     then(resolve, reject) {
         // 校验一下是否为方法
         typeof resolve === 'function' && this.resolveCbs.push(resolve)
@@ -124,21 +158,22 @@ class DPromise {
 
 let p1 = new DPromise((resolve, reject) => {
     setTimeout(() => {
-        num > 5 ? resolve(num) : reject(new Error('num小于5'))
+        num > 5 ? resolve(num) : reject('num小于5')
     }, 1500)
 })
 
-/* p1.then(resolve => {
+p1.then(resolve => {
     console.log('resolve', resolve);
     return resolve + 1
 }, err => {
-    console.error(err)
+    console.log('reject', err)
 })
 .then(res => console.log('res2', res))
-.catch(err => console.log('--error--', err)) */
+.catch(err => console.log('--error--', err))
 
 // --------测试DPromise.all
-let p2 = new DPromise(resolve => {
+/* let p2 = new DPromise(resolve => {
+    // console.log('p2-this', this);
     resolve(2)
 })
 
@@ -160,4 +195,4 @@ DPromise.all([p2, p3, p4])
 })
 .catch(err => {
     console.log('promise.all-err', err)
-})
+}) */
