623da06fc108896c2d90ad6d5b02f676c437e252
xiaodongrui <981926563@qq.com>
2020-02-02 22:54:54 +0800

    ts打包配置修改

--- websocket/webpack.config.js
+++ websocket/webpack.config.js
@@ -15,23 +15,24 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/,
-            },
-            {
-                // 使用babel 
-                test: /\.js$/, 
+                // 使用babel 处理 ts
+                test: /\.(js|tsx?)$/, 
                 exclude: /node_modules/, // 忽略依赖中的文件
                 use: [
                     {
                         loader: "babel-loader",
                     },
                 ]
-            }
+            },
+            // ts 的 webpack loader对ts进行处理，也可以使用babel的preset处理ts
+            // {
+            //     test: /\.ts?$/,
+            //     loader: 'awesome-typescript-loader',
+            //     exclude: /node_modules/,
+            // },
         ]
     },
     plugins: [
-        new CheckerPlugin()
+        // new CheckerPlugin()
     ]
 }
